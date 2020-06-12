9a3e65c8be587627577852491dd0a3942c751044
Swaroop Gupta Voleti <vswaroop@athenahealth.com>
2020-06-12 23:02:49 +0530

    initial commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,69 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import { flushChunkNames } from 'react-universal-component/server';
-import flushChunks from 'webpack-flush-chunks';
-import Helmet from 'react-helmet';
-import App from '../../shared/App';
-import axios from 'axios';
-import { StaticRouter } from 'react-router';
+import express from 'express';
 
-/**
- * Provides the server side rendered app. In development environment, this method is called by
- * `react-hot-server-middleware`.
- *
- * This method renders the ejs template `public/views/index.ejs`.
- * @param clientStats Parameter passed by hot server middleware
- */
+// we'll talk about this in a minute:
+import serverRenderer from './middleware/renderer';
 
-export default ({ clientStats }) => async (request, response) => {
-  var callAPI = 'https://hn.algolia.com/api/v1/search?tags=front_page';
-  axios.get(callAPI)
-    .then(data => {
-      const globalData = data.data;
-      const pageJson = data.data.hits;
-      const app = (
-        <div className="ahc-redesign">
-          <StaticRouter location={request.url} context={{}}>
-            <main className="main-wrapper">
-              <App pageJson={pageJson} globalData={globalData} />
-            </main>
-          </StaticRouter>
-        </div>
-      );
-      const appString = ReactDOM.renderToString(app);
-      const chunkNames = flushChunkNames();
-      const { js, styles, cssHash } = flushChunks(clientStats, { chunkNames });
+const PORT = 3000;
+const path = require('path');
 
-      //Setting up meta information for SEO.
-      const helmet = Helmet.renderStatic();
+// initialize the application and create the routes
+const app = express();
+const router = express.Router();
 
-      // TODO use javascript serializer for XSS attack.
-      response.render('index', {
-        title: helmet.title.toString(),
-        meta: helmet.meta.toString(),
-        link: helmet.link.toString(),
-        appString,
-        js,
-        styles,
-        cssHash,
-        pageJson: pageJson,
-        globalData: globalData,
-        callAPI: JSON.stringify(callAPI),
-      })
+// root (/) should always serve our server rendered page
+router.use('^/$', serverRenderer);
 
-    })
-    .catch((error) => {
-      // Error
-      if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the
-        // browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-    });
-};
+// other static resources should just be served as they are
+router.use(express.static(
+  path.resolve(__dirname, '..', 'build'),
+  { maxAge: '30d' },
+));
+
+// tell the app to use the above rules
+app.use(router);
+
+// start the app
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log('something bad happened', error);
+  }
+
+  console.log("listening on " + PORT + "...");
+});
